c03c2d9fa97f28a063616f17936a2c2e7dc3f4d9
hoodboy <littlepark0107@gmail.com>
2021-10-29 16:27:03 +0900

    feat: add AlertModal

--- src/pages/MemberInfo.js
+++ src/pages/MemberInfo.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, {useState} from 'react';
+import styled, {css, keyframes} from 'styled-components';
 import {PageWrapper} from '../components/Pagestyles';
 import ArrowImg from '../assets/arrow.png';
+import {Link} from 'react-router-dom';
 
 const MemberInfo = () => {
+
+    const [goBack, SetGoBack] = useState(false);
+
+    const onGoBack = () => {
+        SetGoBack(!goBack)
+    }
+
     return (
         <PageWrapper>
             <Header>
-                <BackArrow/>
-                기본 정보
+                <ArrowWrapper onClick={onGoBack} >
+                    <BackArrow/>
+                    기본 정보
+                </ArrowWrapper>
             </Header>
             <InfoInuptWrapper>
                 <Nikname>
@@ -29,6 +39,21 @@ const MemberInfo = () => {
                 </CertifyWrapper>
             </InfoInuptWrapper>
             <CompletionButton>회원가입 완료</CompletionButton>
+            <BackAltert open={goBack}>
+                <Opacity onClick={onGoBack}/>
+                <AlertModal>
+                    <AlertTitle>
+                        회원가입을 중단하시겠습니까?
+                    </AlertTitle>
+                    <Line/>
+                    <AlertSelectWrapper>
+                        <AlertSelect onClick={onGoBack}>아니오</AlertSelect>
+                        <Link to="/start" style={{textDecoration: "none", color: "#000"}}>
+                            <AlertSelect>예</AlertSelect>
+                        </Link>
+                    </AlertSelectWrapper>
+                </AlertModal>
+            </BackAltert>
         </PageWrapper>
     )
 }
@@ -53,6 +78,10 @@ const BackArrow = styled.div`
     background-repeat: no-repeat;
     background-position: center;
     margin-right: 20px;
+`;
+
+const ArrowWrapper = styled.div`
+    display: flex;
 `;
 
 const InfoInuptWrapper = styled.div`
@@ -127,6 +156,64 @@ const CompletionButton = styled.div`
     font-size: 16px;
     position: absolute;
     bottom: 12%;
+`;
+
+const BackAltert = styled.div`
+    position: absolute;
+    display: none;
+    ${props => props.open && css`
+        display:flex;
+    `}
+`;
+
+const Opacity = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: #000;
+    opacity: 0.2;
+    z-index: 2;
+`;
+
+const AlertModal = styled.div`
+    width: 80vw;
+    height: 15vh;
+    padding: 0vw 5vw;
+    background: #fff;
+    z-index: 3;
+    position: absolute;
+    top: 40%;
+    left: 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+`;
+
+const Line = styled.div`
+    width: 100%;
+    margin-top: 7%;
+    border-bottom: 1px solid #707070;
+`;
+
+const AlertTitle = styled.div`
+    margin-top: 7%;
+`;
+
+const AlertSelectWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    text-align: center;
+    justify-content: space-around;
+    margin-bottom: 7%;
+`;
+
+const AlertSelect = styled.div`
+    width: 100px;
+    height: 200%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default MemberInfo;
