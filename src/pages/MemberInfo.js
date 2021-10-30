19ce2b8d8626d064aafc9adda165a908802563b3
hoodboy <littlepark0107@gmail.com>
2021-10-30 20:10:56 +0900

    feat: member info publishing

--- src/pages/MemberInfo.js
+++ src/pages/MemberInfo.js
@@ -3,13 +3,18 @@ import styled, {css, keyframes} from 'styled-components';
 import {PageWrapper} from '../components/Pagestyles';
 import ArrowImg from '../assets/arrow.png';
 import {Link} from 'react-router-dom';
+import RightArrowImg from '../assets/rightarrow.png';
 
 const MemberInfo = () => {
 
     const [goBack, SetGoBack] = useState(false);
-
     const onGoBack = () => {
         SetGoBack(!goBack)
+    }
+
+    const [done, setDone] = useState(false);
+    const onDone = () => {
+        setDone(!done)
     }
 
     return (
@@ -17,47 +22,77 @@ const MemberInfo = () => {
             <Header>
                 <ArrowWrapper onClick={onGoBack} >
                     <BackArrow/>
-                    기본 정보
+                    개인 등록
                 </ArrowWrapper>
             </Header>
-            <InfoInuptWrapper>
-                <Nikname>
-                    <NiknameTitle>닉네임</NiknameTitle>
-                    <NiknameInput placeholder="닉네임을 입력해주세요"></NiknameInput>
-                </Nikname>
-                <PhoneNumber>
-                    <PhoneNumberTitle>전화번호</PhoneNumberTitle>
-                    <PhoneInputWrapper>
-                        <PhoneInput type="number" placeholder="전화번호를 입력해주세요" maxlength="9"></PhoneInput>
-                        <PhoneButton>인증요청</PhoneButton>
-                    </PhoneInputWrapper>
-                </PhoneNumber>
-                <CertifyWrapper>
-                    <CertifyInput placeholder="인증번호를 입력해주세요">
-
-                    </CertifyInput>
-                </CertifyWrapper>
-            </InfoInuptWrapper>
-            <CompletionButton>
-                <Link to="/main" style={{textDecoration: "none", color: "#fff", width: "100%", height: "100%", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                    회원가입 완료
-                </Link>
+            <ResevationBlock>
+                <ResevationTitle>개인 풋살 정보</ResevationTitle>
+                <BookerWrapper>
+                    <NameInput placeholder="이름을 입력해주세요."></NameInput>
+                    <ContactInput placeholder="연락처를 입력해주세요."></ContactInput>
+                    <Link to="/member-info" style={{textDecoration: "none"}}>
+                        <TimeInput>
+                            <InputTitle>풋살 레벨을 선택해주세요.</InputTitle>
+                            <RightArrow/>
+                        </TimeInput>
+                    </Link>               
+                </BookerWrapper>
+                <DateTitle>희망 풋살 매칭 일시</DateTitle>
+                <DateWrapper>
+                    <Link to="/member-info" style={{textDecoration: "none"}}>
+                        <TimeInput>
+                            <InputTitle>희망 풋살 매칭 일시를 선택해주세요.</InputTitle>
+                            <RightArrow/>
+                        </TimeInput>
+                    </Link> 
+                </DateWrapper>
+                <LocationTitle>활동 지역</LocationTitle>
+                <LocationWrapper>
+                    <Link to="/member-info" style={{textDecoration: "none"}}>
+                        <TimeInput>
+                            <InputTitle>지역을 선택해주세요.</InputTitle>
+                            <RightArrow/>
+                        </TimeInput>
+                    </Link>
+                </LocationWrapper>
+            </ResevationBlock>
+            <Notice>
+                * 개인 등록에 대한 안내 및 주의사항입니다.<br/>
+                * 매칭 연결를 위해 개인정보를 수집합니다.
+            </Notice>
+            <CompletionButton onClick={onDone}>
+                개인 정보 등록 완료
             </CompletionButton>
             <BackAltert open={goBack}>
                 <Opacity onClick={onGoBack}/>
                 <AlertModal>
                     <AlertTitle>
-                        회원가입을 중단하시겠습니까?
+                        개인 정보 등록을 취소하시겠습니까?
                     </AlertTitle>
                     <Line/>
                     <AlertSelectWrapper>
                         <AlertSelect onClick={onGoBack}>아니오</AlertSelect>
-                        <Link to="/start" style={{textDecoration: "none", color: "#000"}}>
+                        <Link to="/main" style={{textDecoration: "none", color: "#000"}}>
                             <AlertSelect>예</AlertSelect>
                         </Link>
                     </AlertSelectWrapper>
                 </AlertModal>
             </BackAltert>
+            <DoneAltert done={done}>
+                <DoneOpacity onClick={onDone}/>
+                <AlertModal>
+                    <AlertTitle>
+                        신청하신 예약정보 확인 후 <br/>
+                        카카오톡으로 안내 드리겠습니다.
+                    </AlertTitle>
+                    <Line/>
+                    <AlertSelectWrapper>
+                        <Link to="/main" style={{textDecoration: "none", color: "#000"}}>
+                            <AlertSelect>확인</AlertSelect>
+                        </Link>
+                    </AlertSelectWrapper>
+                </AlertModal>
+            </DoneAltert>
         </PageWrapper>
     )
 }
@@ -89,73 +124,77 @@ const ArrowWrapper = styled.div`
     display: flex;
 `;
 
-const InfoInuptWrapper = styled.div`
+const ResevationBlock = styled.div`
+    width: 100%;
+    height: 88vh;
+    background: #F2F3F5;
+`;
+
+const ResevationTitle = styled.div`
+    margin: 18px 5vw 15px 5vw;
+    font-size: 14px;
+`;
+
+const DateTitle = styled(ResevationTitle)`
+    margin: 28px 5vw 15px 5vw;
+`;
+
+const LocationTitle = styled(DateTitle)`
 
 `;
 
-const Nikname = styled.div`
-    margin: 20px auto;
+const BookerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-top: 0.4px solid #707070;
+    border-bottom: 0.4px solid #707070;
+    background: #fff;
 `;
 
-const NiknameTitle = styled.div`
+const DateWrapper = styled(BookerWrapper)`
 
 `;
 
-const NiknameInput = styled.input`
+const LocationWrapper = styled(BookerWrapper)`
+
+`;
+
+const NameInput = styled.input`
     border: none;
     outline: none;
+    margin: 0 5vw;
+    padding: 4vw 0;
+    border-bottom: 0.4px solid #707070;
     font-size: 14px;
-    padding: 20px 0;
-    width: 90vw;
-    border-bottom: 1px solid #AFB4BE;
+    color: #4B4C4D;
+    ::placeholder {color:#4B4C4D;}
 `;
 
-const PhoneNumber = styled.div`
-    margin: 10px auto;
+const ContactInput = styled(NameInput)`
+
 `;
 
-const PhoneNumberTitle = styled.div`
-    margin-top: 30px;
+const TeamInput = styled(NameInput)`
+    border: none;
 `;
 
-const PhoneInputWrapper = styled.div`
+const DateInput = styled.div`
     display: flex;
-    width: 90vw;
-    align-items: flex-end;
-`;
-
-const PhoneInput = styled(NiknameInput)`
-    width: 65%;
-`;
-
-const PhoneButton = styled.div`
-    width: 30%;
-    height: 50px;
-    background: #C9E8D6;
-    border-radius: 100px;
-    color: #fff;
-    font-size: 16px;
-    display: flex;
-    justify-content: center;
+    margin: 0 5vw;
+    padding: 4vw 0;
+    border-bottom: 0.4px solid #707070;
     align-items: center;
-    margin-left: 5%;
-    &:active {
-      transform: scale(0.98);
-    }
+    justify-content: space-between;
 `;
 
-const CertifyWrapper = styled.div`
-
-`;
-
-const CertifyInput = styled(NiknameInput)`
-
+const TimeInput = styled(DateInput)`
+    border: none;
 `;
 
 const CompletionButton = styled.div`
     width: 90vw;
     height: 50px;
-    background: #40B65E;
+    background: #C9E8D6;
     border-radius: 100px;
     display: flex;
     justify-content: center;
@@ -169,6 +208,16 @@ const CompletionButton = styled.div`
       transform: scale(0.98);
     }
 `;
+
+const Notice = styled.div`
+    position: absolute;
+    bottom: 20%;
+    font-size: 14px;
+    color: #4B4C4D;
+    width: 85%;
+    line-height: 24px;
+`;
+
 
 const BackAltert = styled.div`
     position: absolute;
@@ -227,5 +276,36 @@ const AlertSelect = styled.div`
     justify-content: center;
     align-items: center;
 `;
+
+const RightArrow= styled.div`
+    width: 8px;
+    height: 16px;
+    background-image: url(${RightArrowImg});
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    margin-right: 8px;
+`;
+
+const InputTitle = styled.div`
+    font-size: 14px;
+    color: #4B4C4D;
+`;
+
+
+const DoneAltert = styled(BackAltert)`
+    ${props => props.done && css`
+        display:flex;
+    `}
+`;
+
+const DoneOpacity = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: #000;
+    opacity: 0.2;
+    z-index: 2;
+`;
+
 
 export default MemberInfo;
