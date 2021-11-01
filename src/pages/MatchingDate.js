16ee12235509a3736b075138c02a7f38f2890316
hoodboy <littlepark0107@gmail.com>
2021-11-01 20:47:40 +0900

    feat: matching date style extraction

--- src/pages/MatchingDate.js
+++ src/pages/MatchingDate.js
@@ -1,7 +1,30 @@
 import React, {useState} from 'react';
 import styled, {css, keyframes} from 'styled-components';
-import {PageWrapper} from '../components/Pagestyles';
-import ArrowImg from '../assets/arrow.png';
+import {
+    PageWrapper,
+    Header,
+    ArrowWrapper,
+    BackArrow,
+    PageBlock,
+    InputBlockWrapper,
+    InputBlock,
+    LastButtonInput,
+    InputTitle,
+    RightArrow,
+    FirstInputBlockTitle,
+    InputBlockTitle,
+    Notice,
+    CompletionButton,
+    BackAltert,
+    Opacity,
+    AlertModal,
+    AlertTitle,
+    Line,
+    AlertSelectWrapper,
+    AlertSelect,
+    DoneAltert,
+    DoneOpacity,
+} from '../components/Pagestyles';import ArrowImg from '../assets/arrow.png';
 import {Link} from 'react-router-dom';
 import RightArrowImg from '../assets/rightarrow.png';
 
@@ -25,32 +48,32 @@ const MatchingDate = () => {
                     희망 풋살 매칭 일시
                 </ArrowWrapper>
             </Header>
-            <ResevationBlock>
-                <ResevationTitle>매칭 요일</ResevationTitle>
-                <BookerWrapper>
+            <PageBlock>
+                <FirstInputBlockTitle>매칭 요일</FirstInputBlockTitle>
+                <InputBlockWrapper>
                     <Link to="/matching-date" style={{textDecoration: "none"}}>
-                        <TimeInput>
+                        <LastButtonInput>
                             <InputTitle>매칭 요일을 선택해주세요.</InputTitle>
                             <RightArrow/>
-                        </TimeInput>
+                        </LastButtonInput>
                     </Link>
-                </BookerWrapper>
-                <DateTitle>희망 매칭 일시 </DateTitle>
-                <DateWrapper>
+                </InputBlockWrapper>
+                <FirstInputBlockTitle>희망 매칭 일시 </FirstInputBlockTitle>
+                <InputBlockWrapper>
                     <Link to="/matching-date" style={{textDecoration: "none"}}>
-                        <TimeInput style={{ borderBottom: "1px solid #707070"}}>
+                        <LastButtonInput style={{ borderBottom: "1px solid #707070"}}>
                             <InputTitle>매칭 시작 시간을 선택해주세요.</InputTitle>
                             <RightArrow/>
-                        </TimeInput>
+                        </LastButtonInput>
                     </Link>
                     <Link to="/matching-date" style={{textDecoration: "none"}}>
-                        <TimeInput>
+                        <LastButtonInput>
                             <InputTitle>매칭 종료 시간을 선택해주세요.</InputTitle>
                             <RightArrow/>
-                        </TimeInput>
+                        </LastButtonInput>
                     </Link>
-                </DateWrapper>
-            </ResevationBlock>
+                </InputBlockWrapper>
+            </PageBlock>
             <CompletionButton onClick={onDone}>
                 매칭일시 입력 완료
             </CompletionButton>
@@ -87,199 +110,5 @@ const MatchingDate = () => {
         </PageWrapper>
     )
 }
-
-const Header = styled.div`
-    width: 90vw;
-    height: 9vh;
-    padding: 3% 5%;
-    background: #40B65E;
-    display: flex;
-    align-items: flex-end;
-    font-size: 15px;
-    color: #fff;
-    box-shadow: rgb(0 0 0 / 10%) 0px 3px 6px;
-`;
-
-const BackArrow = styled.div`
-    width: 8px;
-    height: 16px;
-    background-image: url(${ArrowImg});
-    background-size: 100%;
-    background-repeat: no-repeat;
-    background-position: center;
-    margin-right: 20px;
-    margin-top: 3px;
-`;
-
-const ArrowWrapper = styled.div`
-    display: flex;
-`;
-
-const ResevationBlock = styled.div`
-    width: 100%;
-    height: 88vh;
-    background: #F2F3F5;
-`;
-
-const ResevationTitle = styled.div`
-    margin: 18px 5vw 15px 5vw;
-    font-size: 14px;
-`;
-
-const DateTitle = styled(ResevationTitle)`
-    margin: 28px 5vw 15px 5vw;
-`;
-
-const LocationTitle = styled(DateTitle)`
-
-`;
-
-const BookerWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    border-top: 0.4px solid #707070;
-    border-bottom: 0.4px solid #707070;
-    background: #fff;
-`;
-
-const DateWrapper = styled(BookerWrapper)`
-
-`;
-
-const LocationWrapper = styled(BookerWrapper)`
-
-`;
-
-const DateInput = styled.div`
-    display: flex;
-    margin: 0 5vw;
-    padding: 4vw 0;
-    border-bottom: 0.4px solid #707070;
-    align-items: center;
-    justify-content: space-between;
-`;
-
-const TimeInput = styled(DateInput)`
-    border: none;
-`;
-
-const CompletionButton = styled.div`
-    width: 90vw;
-    height: 50px;
-    background: #40B65E;
-    border-radius: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    font-size: 16px;
-    position: fixed;
-    bottom: 12vh;
-    left: 5%;
-    &:active {
-      transform: scale(0.98);
-    }
-`;
-
-const Notice = styled.div`
-    position: absolute;
-    bottom: 20%;
-    font-size: 14px;
-    color: #4B4C4D;
-    width: 85%;
-    line-height: 24px;
-`;
-
-
-const BackAltert = styled.div`
-    position: absolute;
-    display: none;
-    min-height: 135px;
-    ${props => props.open && css`
-        display:flex;
-    `}
-`;
-
-const Opacity = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: #000;
-    opacity: 0.2;
-    z-index: 2;
-`;
-
-const AlertModal = styled.div`
-    width: 80vw;
-    height: 15vh;
-    min-height: 135px;
-    padding: 0vw 5vw;
-    background: #fff;
-    z-index: 3;
-    position: absolute;
-    top: 40%;
-    left: 5%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    text-align: center;
-`;
-
-const Line = styled.div`
-    width: 100%;
-    margin-top: 7%;
-    border-bottom: 1px solid #707070;
-`;
-
-const AlertTitle = styled.div`
-    margin-top: 7%;
-`;
-
-const AlertSelectWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    text-align: center;
-    justify-content: space-around;
-    margin-bottom: 7%;
-`;
-
-const AlertSelect = styled.div`
-    width: 100px;
-    height: 200%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-const RightArrow= styled.div`
-    width: 8px;
-    height: 16px;
-    background-image: url(${RightArrowImg});
-    background-size: 100%;
-    background-repeat: no-repeat;
-    background-position: center;
-    margin-right: 8px;
-`;
-
-const InputTitle = styled.div`
-    font-size: 14px;
-    color: #4B4C4D;
-`;
-
-
-const DoneAltert = styled(BackAltert)`
-    ${props => props.done && css`
-        display:flex;
-    `}
-`;
-
-const DoneOpacity = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background: #000;
-    opacity: 0.2;
-    z-index: 2;
-`;
-
 
 export default MatchingDate;
