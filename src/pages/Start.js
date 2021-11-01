c59ab30185b57f6b790943e269983eda946bd570
hoodboy <littlepark0107@gmail.com>
2021-11-01 21:52:49 +0900

    fix: start waring

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import {PageWrapper} from '../components/Pagestyles';
 import StartLogoImg from '../assets/startlogo.png'
