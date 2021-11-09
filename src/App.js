b72bd742341d3539934b3d74ebbccc20398e98b3
hoodboy <littlepark0107@gmail.com>
2021-11-09 14:57:45 +0900

    feat: add affiliated-team page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import "./fonts/pretendard/index.css";
 import Splash from './pages/Splash';
 import Start from './pages/Start';
@@ -16,29 +16,31 @@ import TeamLeader from './pages/TeamLeader';
 import TeamMember from './pages/TeamMember';
 import MatchingDate from './pages/MatchingDate';
 import TeamRevise from './pages/TeamRevise';
+import AffiliatedTeam from './pages/AffiliatedTeam';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Splash} exact />
-        <Route path="/start" component={Start} />
-        <Route path="/register" component={Register} />
-        <Route path="/member-info" component={MemberInfo}/>
-        <Route path="/main" component={Main} />
-        <Route path="/reservation" component={Reservation} />
-        <Route path="/matching" component={Matching} />
-        <Route path="/team-register" component={TeamRegister} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/setting" component={Setting} />
-        <Route path="/matching-team" component={MatchingTeam} />
-        <Route path="/team-leader" component={TeamLeader} />
-        <Route path="/team-member" component={TeamMember} />
-        <Route path="/matching-date" component={MatchingDate} />
-        <Route path="/team-revise" component={TeamRevise} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={Splash} exact />
+                <Route path="/start" component={Start} />
+                <Route path="/register" component={Register} />
+                <Route path="/member-info" component={MemberInfo} />
+                <Route path="/main" component={Main} />
+                <Route path="/reservation" component={Reservation} />
+                <Route path="/matching" component={Matching} />
+                <Route path="/team-register" component={TeamRegister} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/setting" component={Setting} />
+                <Route path="/matching-team" component={MatchingTeam} />
+                <Route path="/team-leader" component={TeamLeader} />
+                <Route path="/team-member" component={TeamMember} />
+                <Route path="/matching-date" component={MatchingDate} />
+                <Route path="/team-revise" component={TeamRevise} />
+                <Route path="/affiliated-team" component={AffiliatedTeam} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
